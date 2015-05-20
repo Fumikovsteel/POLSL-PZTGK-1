5f479bd5e6eabc12c28c30439035b1fa94988b61
rafiten <rafal.pomocnik@gmail.com>
2015-05-20 23:46:48 +0200

    Added UnityTestTools with first integration tests (enemy spawning and shooting)

---
+++ Project/Assets/Testing/IntegrationTests/PassWhenGameObjectFound.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[IntegrationTest.DynamicTestAttribute("ExampleIntegrationTests")]
+// [IntegrationTest.Ignore]
+[IntegrationTest.ExpectExceptions(false, typeof(ArgumentException))]
+[IntegrationTest.SucceedWithAssertions]
+[IntegrationTest.TimeoutAttribute(1)]
+[IntegrationTest.ExcludePlatformAttribute(RuntimePlatform.Android, RuntimePlatform.LinuxPlayer)]
+public class PassWhenGameObjectFound : MonoBehaviour {
+
+	public string gameObjectName = "";
+
+	void Update() 
+	{
+		GameObject gameObject = GameObject.Find (gameObjectName);
+		if (gameObject != null)
+			IntegrationTest.Pass (gameObject);
+	}
+}
