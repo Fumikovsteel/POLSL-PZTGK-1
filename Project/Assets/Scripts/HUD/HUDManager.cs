bb7020b5b8bb4a06c0a35e2337e60737c4af11bc
Michał Rusin <michalrusin016@gmail.com>
2015-05-26 22:06:35 +0200

    Improved HUD

--- Project/Assets/Scripts/HUD/HUDManager.cs
+++ Project/Assets/Scripts/HUD/HUDManager.cs
@@ -6,10 +6,28 @@ public class HUDManager : MonoBehaviour
 {
     [SerializeField]
     private Text healthText;
+    [SerializeField]
+    private RectTransform healthTransform;
+
+    [SerializeField]
+    private GameObject sword1Image;
+    [SerializeField]
+    private GameObject sword2Image;
+    [SerializeField]
+    private GameObject shield1Image;
+    [SerializeField]
+    private GameObject shield2Image;
+    [SerializeField]
+    private GameObject armor1Image;
+    [SerializeField]
+    private GameObject armor2Image;
 
     private void Awake()
     {
         Zelda._Game._GameManager._Player._OnHealthChanged += OnHealthChanged;
+        sword2Image.SetActive(false);
+        shield2Image.SetActive(false);
+        armor2Image.SetActive(false);
     }
 
     private void OnDestroy()
@@ -21,5 +39,36 @@ public class HUDManager : MonoBehaviour
     private void OnHealthChanged(int life, int maxLife)
     {
         healthText.text = life.ToString() + '/' + maxLife.ToString();
+        float fMaxLife = (float)maxLife;
+        float fLife = (float)life;
+
+        if (fLife >= 0)
+        {
+            float xPosition = 0f - ((fMaxLife - fLife) / fMaxLife) * 430f;
+
+            Vector2 pos = healthTransform.anchoredPosition;
+
+            pos.x = xPosition;
+            
+            healthTransform.anchoredPosition = pos;
+        }
+    }
+
+    private void showAdvancedSword()
+    {
+        sword1Image.SetActive(false);
+        sword2Image.SetActive(true);
+    }
+
+    private void showAdvancedShield()
+    {
+        shield1Image.SetActive(false);
+        shield2Image.SetActive(true);
+    }
+
+    private void showAdvancedArmor()
+    {
+        armor1Image.SetActive(false);
+        armor2Image.SetActive(true);
     }
 }
