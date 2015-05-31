a7091fd1ce1f4e905e88d10d733bd2ecb9cc6a2f
Tadeusz <tadeusz.gromadzki@gmail.com>
2015-05-31 22:34:22 +0200

    Added Sound Manager and various sounds
    Added Sound Manager
    Added Main Menu Theme
    Added Background Musics to the 1st level with fading effects

---
+++ Project/Assets/Scripts/Audio/BGMTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider zone)
+	{
+		SoundManager.instance.PlayMusic(zone);
+	}
+
+	void OnTriggerExit(Collider zone)
+	{
+		SoundManager.instance.MusicFadeOut(zone);
+	}
+}
---
+++ Project/Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+
+	public AudioSource musicSource;
+	public AudioSource musicSource2;
+	public AudioSource efxSource;
+	public static SoundManager instance = null;
+	public bool fading1 = false;
+	public bool fading2 = false;
+	public AudioClip VillageBGM;
+	public AudioClip ForestBGM;
+	public AudioClip CatacombEntranceBGM;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlaySound(AudioClip clip)
+	{
+		efxSource.clip = clip;
+		efxSource.Play();
+	}
+
+	public void PlayMusic(Collider zone)
+	{
+		if (zone.CompareTag("VillageAudioTrigger"))
+		{
+			musicSource.volume = 0;
+			musicSource.clip = VillageBGM;
+			musicSource.Play ();
+			fading1 = false;
+		}
+		if (zone.CompareTag("ForestAudioTrigger"))
+		{
+			musicSource2.volume = 0;
+			musicSource2.clip = ForestBGM;
+			musicSource2.Play ();
+			fading2 = false;
+		}
+		if (zone.CompareTag("CatacombEntranceAudioTrigger"))
+		{
+			musicSource.volume = 0;
+			musicSource.clip = CatacombEntranceBGM;
+			musicSource.Play ();
+			fading1 = false;
+		}
+	}
+
+	public void MusicFadeOut(Collider zone)
+	{
+		if (zone.CompareTag("ForestAudioTrigger"))
+			fading2 = true;
+		else
+			fading1 = true;
+	}
+
+	void Update()
+	{
+		if (!fading1 && musicSource.volume < 1)
+		{
+			musicSource.volume += 0.5f * Time.deltaTime;
+		}
+
+		if (fading1 && musicSource.volume > 0)
+		{
+			musicSource.volume -= 0.5f * Time.deltaTime;
+		}
+
+		if (!fading2 && musicSource2.volume < 1)
+		{
+			musicSource2.volume += 0.5f * Time.deltaTime;
+		}
+		
+		if (fading2 && musicSource2.volume > 0)
+		{
+			musicSource2.volume -= 0.5f * Time.deltaTime;
+		}
+	}
+}
